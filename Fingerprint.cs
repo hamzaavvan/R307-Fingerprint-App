1821c11fb316bc63b6ff1e0c2b0755557a56e800
hamzaavvan <hamzaavvan@gmail.com>
2020-01-26 18:08:19 +0500

    Code Update

--- Fingerprint.cs
+++ Fingerprint.cs
@@ -12,43 +12,57 @@ namespace WindowsFormsApp1
     class Fingerprint
     {
         SerialPort port;
-        byte[] cmd = new byte[512];
+        byte   package_identifier;
+        byte[] package = new byte[512];
+        byte[] package_len = new byte[2];
+        byte[] checksum = new byte[2];
+        byte[] package_content = new byte[512];
 
         public Fingerprint()
         {
             port = new SerialPort("COM4", 9600);
             port.Open();
+
+            this.set_header();
+            this.set_adder();
         }
 
         private void set_header()
         {
-            cmd[0] = 0xEF;
-            cmd[1] = 0x01;
+            package[0] = 0xEF;
+            package[1] = 0x01;
         }
 
         private void set_adder()
         {
-            cmd[2] = 0xff;
-            cmd[3] = 0xff;
-            cmd[4] = 0xff;
-            cmd[5] = 0xff;
+            package[2] = 0xff;
+            package[3] = 0xff;
+            package[4] = 0xff;
+            package[5] = 0xff;
         }
 
-        private void set_package_length(int data_packet_len, int cmd_packet_len, int checksum_len)
+        private void set_package_length(int packet_len, int checksum_len)
         {
-            byte[] package_len = BitConverter.GetBytes(data_packet_len + cmd_packet_len + checksum_len);
+            this.package_len = BitConverter.GetBytes(packet_len + checksum_len);
 
-            Buffer.BlockCopy(package_len, 0, cmd, 6, package_len.Length);
+            Buffer.BlockCopy(package_len, 0, package, package.Length-1, package_len.Length);
         }
 
-        private void set_package_content()
+        private void set_package_content(string data)
         {
+            this.package_content = Encoding.ASCII.GetBytes(data);
 
+            Buffer.BlockCopy(package_content, 0, package, package.Length - 1, package_content.Length);
         }
 
-        private void checksum()
+        private void set_checksum()
         {
+            Buffer.BlockCopy(this.checksum, 0, package, package.Length - 1, this.checksum.Length);
+        }
 
+        private void calculate_checksum()
+        {
+            this.checksum = BitConverter.GetBytes(this.package_len.Length + this.package_content.Length + this.package_identifier);
         }
 
         /**
@@ -60,10 +74,41 @@ namespace WindowsFormsApp1
          * 07h for acknowledge package
          * 08h for end of data packet
          */
-        private void package_identifier(byte code = 0x1)
+        private void set_package_identifier(byte code = 0x1)
         {
-            cmd[6] = code;
+            this.package_identifier = code;
+            package[6] = code;
         }
+
+        private void init(byte identifier, string _string)
+        {
+            this.set_package_identifier(identifier);
+            this.calculate_checksum();
+            this.set_package_length(_string.Length, this.checksum.Length);
+            this.set_package_content(_string);
+            this.set_checksum();
+        }
+
+        public void send_command(string cmd)
+        {
+            this.init(0x1, cmd);
+        }
+
+        public void send_data(string data)
+        {
+            this.init(0x2, data);
+        }
+
+        public void send_acknowledgement(string acknowledgement)
+        {
+            this.init(0x7, acknowledgement);
+        }
+
+        public void end(string data = "")
+        {
+            this.init(0x8, data);
+        }
+
 
         public Bitmap write(string text = "")
         {
